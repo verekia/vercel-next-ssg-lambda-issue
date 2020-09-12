f6489b3eea6ab003b89697d03e8f7a1af0f8c444
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-09-12 15:15:53 +0200

    Add a prop and typedef

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
-const AboutPage = () => <div>About</div>
+import { GetStaticProps } from 'next'
 
-export const getStaticProps = async () => ({ props: {} })
+const AboutPage = ({ foo }) => <div>About, {foo}</div>
+
+export const getStaticProps: GetStaticProps = async () => ({ props: { foo: 'bar' } })
 
 export default AboutPage
