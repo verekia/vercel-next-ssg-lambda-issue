8c69c09b2beb8a2d25aff105ccb776127e97effd
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-09-12 16:08:14 +0200

    Add doesntexist import

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next'
 
+// @ts-ignore
+import doesntexist from 'doesntexist'
+
 const AboutPage = () => <div>About</div>
 
 export const getStaticProps: GetStaticProps = async () => {
-
   return { props: {} }
 }
 
