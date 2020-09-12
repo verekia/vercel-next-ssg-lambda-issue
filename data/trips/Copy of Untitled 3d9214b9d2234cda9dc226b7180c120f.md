be177200219e43691642d08d8bfcd8eea4c49b8d
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-09-12 16:05:51 +0200

    Removed data

--- data/trips/Copy of Untitled 3d9214b9d2234cda9dc226b7180c120f.md
+++
@@ -1,9 +0,0 @@
-# Copy of Untitled
-
-Arrival Transport: train
-City: https://www.notion.so/Barcelona-5882f0224140467091cb552e7d47b67a
-Country: https://www.notion.so/Spain-cf145d23cc704a34ab66894692eb8d3e
-Days: 1
-Departure Transport: train
-From: Oct 12, 2017
-To: Oct 13, 2017
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,75 +1,10 @@
-import fs from 'fs'
-
 import { GetStaticProps } from 'next'
 
-import { swit } from '@sharyn/util'
-import yaml from 'js-yaml'
-import { omit } from 'lodash'
-
-const AboutPage = ({ foo }) => <div>About, {foo}</div>
+const AboutPage = () => <div>About</div>
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Be careful of .DS_Store files that break the parsing of all files in the folders
-  const countries = fs.readdirSync('data/countries').map((fileName) => {
-    const [, id] = fileName.match(/.* (.*)\.md/)
-    const md = fs.readFileSync(`data/countries/${fileName}`).toString()
-    const [, name] = md.match(/# (.*)\n/)
-    const emoji = swit(
-      name,
-      ['France', '🇫🇷'],
-      ['Portugal', '🇵🇹'],
-      ['Spain', '🇪🇸'],
-      ['Thailand', '🇹🇭'],
-      ['Vietnam', '🇻🇳'],
-      ['Hong Kong', '🇭🇰'],
-      ['Singapore', '🇸🇬'],
-      ['South Korea', '🇰🇷'],
-      ['Hungary', '🇭🇺'],
-      ['Bulgaria', '🇧🇬'],
-      ['Serbia', '🇷🇸'],
-      ['Ukraine', '🇺🇦'],
-      ['Poland', '🇵🇱'],
-      ['Italy', '🇮🇹'],
-      ['Indonesia', '🇮🇩'],
-      ['Malaysia', '🇲🇾'],
-      '😥'
-    )
-    return { fileName, id, name, emoji }
-  })
-  const cities = fs.readdirSync('data/cities').map((fileName) => {
-    const [, id] = fileName.match(/.* (.*)\.md/)
-    const md = fs.readFileSync(`data/cities/${fileName}`).toString()
-    const [, name] = md.match(/# (.*)\n/)
-    const yamlData = yaml.safeLoad(md)
-    const cleanedUpData = { countryId: yamlData.Country.match(/.*-(.*)$/)[1] }
-    return { fileName, id, name, ...cleanedUpData }
-  })
-  const trips = fs.readdirSync('data/trips').map((fileName) => {
-    const [, id] = fileName.match(/.* (.*)\.md/)
-    const md = fs.readFileSync(`data/trips/${fileName}`).toString()
-    const yamlData = yaml.safeLoad(md)
-    const cleanedUpData = {
-      arrivalTransport: yamlData['Arrival Transport'] ?? null,
-      departureTransport: yamlData['Departure Transport'] ?? null,
-      days: yamlData.Days,
-      fromDate: new Date(yamlData.From),
-      toDate: new Date(yamlData.To),
-      from: yamlData.From,
-      to: yamlData.To,
-      countryId: yamlData.Country.match(/.*-(.*)$/)[1],
-      cityId: yamlData.City.match(/.*-(.*)$/)[1],
-    }
-    return { fileName, id, ...cleanedUpData }
-  })
-  const tripsWithCities = trips
-    .sort((a, b) => b.fromDate.getTime() - a.fromDate.getTime())
-    .map((t) => ({
-      ...omit(t, 'fromDate', 'toDate'),
-      city: cities.find((c) => c.id === t.cityId).name,
-      country: countries.find((c) => c.id === t.countryId).name,
-      emoji: countries.find((c) => c.id === t.countryId).emoji,
-    }))
-  return { props: { trips: tripsWithCities, foo: 'bar' } }
+
+  return { props: {} }
 }
 
 export default AboutPage
